64adf053022cff771e9aa1b9484f1d5a7fabba78
haydenbbickerton <haydenbbickerton@gmail.com>
2023-08-08 20:50:04 +0000

    Add CORS and format

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-import os from 'os';
-import path from 'path';
-import {execa} from 'execa';
-import fs from 'fs-extra';
-const binaryPath = process.env.TAILWIND_BINARY_PATH ?? path.join(__dirname, '..', '..', 'bin', 'tailwindcss-linux-x64');
-
-
+import os from 'os'
+import path from 'path'
+import { execa } from 'execa'
+import fs from 'fs-extra'
+const binaryPath =
+  process.env.TAILWIND_BINARY_PATH ??
+  path.join(__dirname, '..', '..', 'bin', 'tailwindcss-linux-x64')
 
 const dis = [binaryPath]
 // import tailwindcss from 'tailwindcss';
@@ -22,19 +22,22 @@ const dis = [binaryPath]
 
 export async function handler(event, context) {
   try {
-    const postData = JSON.parse(event.body);
+    const postData = JSON.parse(event.body)
 
-    const css = postData.css; 
-    const content = postData.content;
-    const userTailwindConfig = postData.userTailwindConfig || {};
+    const css = postData.css
+    const content = postData.content
+    const userTailwindConfig = postData.userTailwindConfig || {}
 
-    const tempDir = os.tmpdir();
+    const tempDir = os.tmpdir()
 
     // const inputFilePath = path.join(tempDir, 'input.css');
     // fs.writeFileSync(inputFilePath, css);
 
-    const contentFilePath = path.join(tempDir, 'content.html');
-    fs.writeFileSync(contentFilePath, typeof content === 'string' ? content : JSON.stringify(content));
+    const contentFilePath = path.join(tempDir, 'content.html')
+    fs.writeFileSync(
+      contentFilePath,
+      typeof content === 'string' ? content : JSON.stringify(content)
+    )
 
     const configContent = `
         module.exports = {
@@ -42,30 +45,43 @@ export async function handler(event, context) {
             corePlugins: {preflight: false},
             theme: { extend: {...${JSON.stringify(userTailwindConfig)}} },
         };
-    `;
-    const configFilePath = path.join(tempDir, 'config.js');
-    fs.writeFileSync(configFilePath, configContent);
+    `
+    const configFilePath = path.join(tempDir, 'config.js')
+    fs.writeFileSync(configFilePath, configContent)
 
-    const binaryPath = process.env.TAILWIND_BINARY_PATH ?? path.join(__dirname, '..', '..', 'bin', 'tailwindcss-linux-x64');
+    const binaryPath =
+      process.env.TAILWIND_BINARY_PATH ??
+      path.join(__dirname, '..', '..', 'bin', 'tailwindcss-linux-x64')
 
-    const { stdout: generatedCss } = await execa(binaryPath, ['-i', '-', '--config', configFilePath, '--minify'],
-    {
-        input: css
-    });
+    const { stdout: generatedCss } = await execa(
+      binaryPath,
+      ['-i', '-', '--config', configFilePath, '--minify'],
+      {
+        input: css,
+      }
+    )
 
     // const { stdout: generatedCss } = await execa(binaryPath, ['--input', inputFilePath, '--config', configFilePath]);
 
     // cleanup temp files
     // fs.removeSync(inputFilePath);
-    fs.removeSync(contentFilePath);
-    fs.removeSync(configFilePath);
-
+    fs.removeSync(contentFilePath)
+    fs.removeSync(configFilePath)
 
     return {
       statusCode: 200,
-      body: JSON.stringify({css: generatedCss.toString()}),
-    };
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow from anywhere
+      },
+      body: JSON.stringify({ css: generatedCss.toString() }),
+    }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() };
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Allow from anywhere
+      },
+      body: error.toString(),
+    }
   }
 }
